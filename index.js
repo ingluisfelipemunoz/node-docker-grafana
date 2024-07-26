be569e382710c44b1d75904bfc6ddec60a36b7a8
Luis Felipe <ingluisfelipemunoz@gmail.com>
2024-07-26 18:04:19 -0400

    Update Dockerfile to expose port 3105 instead of 3000 Update package.json to start the server on port 3105

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-const express = require('express')
-const Prometheus = require('prom-client')
-const app = express()
-const port = 3000
+const express = require("express");
+const Prometheus = require("prom-client");
+
+const app = express();
+const port = 3105;
 
 const requestCounter = new Prometheus.Counter({
-  name: 'node_request_count',
-  help: 'Number of requests received',
-})
+  name: "node_request_count",
+  help: "Number of requests received",
+});
 
-// to count requests
+const requestDuration = new Prometheus.Histogram({
+  name: "node_request_duration_seconds",
+  help: "Duration of requests in seconds",
+  labelNames: ["method", "path"],
+});
+
+// Middleware to measure request duration
 app.use((req, res, next) => {
-  requestCounter.inc()
-  next()
-})
+  requestCounter.inc();
+  const start = Date.now();
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    requestDuration.observe(
+      { method: req.method, path: req.path },
+      duration / 1000
+    );
+  });
+  next();
+});
 
-app.get('/', (req, res) => {
-  res.send('test test test!')
-})
+app.get("/", (req, res) => {
+  console.log({ message: "holaaa", id: 1 });
+  res.send("test test test!");
+});
 
-// Metrics endpoint for Prometheus
-app.get('/metrics', (req, res) => {
-  res.set('Content-Type', Prometheus.register.contentType)
-  res.end(Prometheus.register.metrics())
-})
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", Prometheus.register.contentType);
+  const metrics = await Prometheus.register.metrics();
+  res.end(metrics);
+});
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
